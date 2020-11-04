cc27f02336471d1fd8360671b60e584d1574df05
Blaire Baker <bakerblaire@gmail.com>
2020-11-04 20:47:46 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,64 @@
+require 'pry'
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total (number)
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game (card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  sum = deal_card + deal_card
+  display_card_total(sum)
+  sum
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
+
+def hit? (card_total)
+  prompt_user
+  response = get_user_input
+  if response == "s"
+    card_total
+  elsif response == "h"
+    card_total += deal_card
+  else
+    invalid_command
+    prompt_user
+    get_user_input
+  end
+end
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  sum = initial_round
+  until sum > 21
+    sum = hit? (sum)
+    display_card_total (sum)
+  end
+  end_game (sum)
 end
-    
